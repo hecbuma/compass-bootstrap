28da00a83330dfcfa7c2c466bbc7422650973806
Hector Bustillos <hector.bustillos@crowdint.com>
2011-09-14 12:35:10 -0500

    add template files

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,2 +1,12 @@
 # Make sure you list all the project template files here in the manifest.
-stylesheet 'screen.sass', :media => 'screen, projection'
+stylesheet 'compass_bootstrap.scss', :media => 'screen, projection'
+stylesheet '_reset.scss', :media => 'screen, projection'
+stylesheet '_preboot.scss', :media => 'screen, projection'
+stylesheet '_scaffolding.scss', :media => 'screen, projection'
+stylesheet '_type.scss', :media => 'screen, projection'
+stylesheet '_tables.scss', :media => 'screen, projection'
+stylesheet '_forms.scss', :media => 'screen, projection'
+stylesheet '_patterns.scss', :media => 'screen, projection'
+
+description "SCSS port of less version of twitter bootstrap"
+
