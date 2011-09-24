81414848328ea0291cbac340fa50dcd2f13de47c
Mario Alberto Chavez <mario.chavez@gmail.com>
2011-09-24 01:04:04 -0500

    Updated to Twitter Bootstrap 1.3.0

--- lib/compass-bootstrap/version.rb
+++ lib/compass-bootstrap/version.rb
@@ -1,5 +1,5 @@
 module Compass
   module Bootstrap
-    VERSION = "0.1.2"
+    VERSION = "0.1.3"
   end
 end
--- templates/compass-bootstrap/manifest.rb
+++ templates/compass-bootstrap/manifest.rb
@@ -1,7 +1,8 @@
 # Make sure you list all the project template files here in the manifest.
 stylesheet 'compass_bootstrap.scss', :media => 'screen, projection'
 stylesheet '_reset.scss', :media => 'screen, projection'
-stylesheet '_preboot.scss', :media => 'screen, projection'
+stylesheet '_variables.scss', :media => 'screen, projection'
+stylesheet '_mixins.scss', :media => 'screen, projection'
 stylesheet '_scaffolding.scss', :media => 'screen, projection'
 stylesheet '_type.scss', :media => 'screen, projection'
 stylesheet '_tables.scss', :media => 'screen, projection'
@@ -9,4 +10,3 @@ stylesheet '_forms.scss', :media => 'screen, projection'
 stylesheet '_patterns.scss', :media => 'screen, projection'
 
 description "SCSS port of less version of twitter bootstrap"
-
