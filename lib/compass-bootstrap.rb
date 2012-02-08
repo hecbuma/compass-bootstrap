ecb414723479daea64768b893d01c58462b55d1d
Eric D. Fields <ericdfields@gmail.com>
2012-02-08 11:23:33 -0500

    base

--- lib/compass-bootstrap.rb
+++ lib/compass-bootstrap.rb
@@ -1,6 +1,6 @@
-require ""
-require "compass-bootstrap/rails"
-require "compass-bootstrap/version"
+require 'compass'
+require 'compass-bootstrap/rails'
+require 'compass-bootstrap/version'
 
 Compass::Frameworks.register('compass-bootstrap',
                              :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
--- lib/compass-bootstrap/version.rb
+++ lib/compass-bootstrap/version.rb
@@ -1,5 +1,5 @@
 module Compass
   module Bootstrap
-    VERSION = "0.1.5"
+    VERSION = "0.1.4"
   end
 end
