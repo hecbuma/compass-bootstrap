239bab2fe4d93241da4db6c55ef4dac4706b8918
Eric D. Fields <ericdfields@gmail.com>
2012-02-08 11:15:09 -0500

    sassied

--- lib/compass-bootstrap.rb
+++ lib/compass-bootstrap.rb
@@ -1,9 +1,7 @@
-require 'compass'
+require ""
 require "compass-bootstrap/rails"
 require "compass-bootstrap/version"
 
-# plugin_root = File.join(File.dirname(__FILE__), "..")
-
 Compass::Frameworks.register('compass-bootstrap',
-                             :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'stylesheets'),
+                             :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
                              :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
--- lib/compass-bootstrap/version.rb
+++ lib/compass-bootstrap/version.rb
@@ -1,5 +1,5 @@
 module Compass
   module Bootstrap
-    VERSION = "0.1.4"
+    VERSION = "0.1.5"
   end
 end
