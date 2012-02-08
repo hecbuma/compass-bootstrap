e85e6a945dba99a36090089252b6b4a4a5fb2add
Eric D. Fields <ericdfields@gmail.com>
2012-02-08 14:30:58 -0500

    responsive

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,5 +1,6 @@
 # Make sure you list all the project template files here in the manifest.
 stylesheet 'compass_bootstrap.scss', :media => 'screen, projection'
+stylesheet 'compass_bootstrap_responsive.scss', :media => 'screen, projection'
 
 discover :javascript
 
