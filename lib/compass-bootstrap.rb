f4ac4540804db4c75e64260b453601f164b1a81d
Eric D. Fields <ericdfields@gmail.com>
2012-02-08 11:51:17 -0500

    todo: set precision

--- lib/compass-bootstrap.rb
+++ lib/compass-bootstrap.rb
@@ -2,6 +2,11 @@ require 'compass'
 require 'compass-bootstrap/rails'
 require 'compass-bootstrap/version'
 
+# TODO
+# Compass::Configuration.add_configuration_property(Sass::Script::Number.precision, "Sets the right amount of decimals for rounding numbers") do
+# 	14
+# end
+
 Compass::Frameworks.register('compass-bootstrap',
                              :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
                              :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
