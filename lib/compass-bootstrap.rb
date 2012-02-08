7a7ead054ec0673ec1fe4326278e7e496e265079
Eric D. Fields <ericdfields@gmail.com>
2012-02-08 09:56:26 -0500

    updated version

--- lib/compass-bootstrap.rb
+++ lib/compass-bootstrap.rb
@@ -4,4 +4,6 @@ require "compass-bootstrap/version"
 
 # plugin_root = File.join(File.dirname(__FILE__), "..")
 
-Compass::Frameworks.register("compass-bootstrap")
+Compass::Frameworks.register('compass-bootstrap',
+                             :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'stylesheets'),
+                             :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
--- lib/compass-bootstrap/version.rb
+++ lib/compass-bootstrap/version.rb
@@ -1,5 +1,5 @@
 module Compass
   module Bootstrap
-    VERSION = "0.1.3"
+    VERSION = "0.1.4"
   end
 end
