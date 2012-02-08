504424edc89adc25e1dc9e0fb1eb8a2789488299
Eric D. Fields <ericdfields@gmail.com>
2012-02-08 09:41:41 -0500

    it compiles!

--- lib/compass-bootstrap.rb
+++ lib/compass-bootstrap.rb
@@ -2,8 +2,6 @@ require 'compass'
 require "compass-bootstrap/rails"
 require "compass-bootstrap/version"
 
-plugin_root = File.join(File.dirname(__FILE__), "..")
+# plugin_root = File.join(File.dirname(__FILE__), "..")
 
-Compass::Frameworks.register("compass-bootstrap", 
-                            :stylesheets_directory => File.join(plugin_root, "stylesheets"),
-                            :templates_directory => File.join(plugin_root, "vendor/assets/stylesheets"))
+Compass::Frameworks.register("compass-bootstrap")
